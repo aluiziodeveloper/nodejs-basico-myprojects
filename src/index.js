ccfc4671fda117056c6d46102db6207b27274a7c
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-07-29 17:42:54 -0300

    create http routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(request, response) {
-    return response.json({
-        message: 'Olá Dev! Bem vindo ao curso!'
-    })
+app.get('/projects', function(request, response) {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2'
+    ])
+})
+
+app.post('/projects', function(request, response) {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ])
+})
+
+app.put('/projects/:id', function(request, response) {
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3'
+    ])
+})
+
+app.delete('/projects/:id', function(request, response) {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3'
+    ])
 })
 
 app.listen(3000, () => {

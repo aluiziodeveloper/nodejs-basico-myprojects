b5a354b34464f42a08cb7b112330584a1d3bd0a7
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-07-30 10:40:32 -0300

    iniciando com APIs

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+
 const app = express()
+app.use(express.json())
 
 // GET http://localhost:3000/projects?title=Node&owner=Aluizio&page=1
-
 // GET http://localhost:3000/projects?page=1&limit=15
 
-app.get('/projects', function(request, response) {
-    const {title, owner, page} = request.query
-    console.log(title, owner, page)
+const projects = []
 
-    return response.json([
-        'Projeto 1',
-        'Projeto 2'
-    ])
+app.get('/projects', function(request, response) {
+    return response.json(projects)
 })
 
 app.post('/projects', function(request, response) {
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3'
-    ])
+    const {name, owner} = request.body
+    const project = {
+        id: uuidv4(),
+        name,
+        owner
+    }
+    projects.push(project)
+
+    return response.status(201).json(project)
 })
 
-app.put('/projects/:id/:name', function(request, response) {
-    const {id, name} = request.params
-    console.log(id, name)
+app.put('/projects/:id', function(request, response) {
+    const {id} = request.params
+    const {name, owner} = request.body
+
+    console.log(id, name, owner)
 
     return response.json([
         'Projeto 4',
